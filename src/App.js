499a4f5b27faaf451f5cf5c3933660f15a2cc995
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-13 18:45:47 +0300

    Сделали возможность удалять данные с сервера. Лоика действий может идити как в прямом, так и в обратном порядке. 1. Создаём deleteProduct, и говорим ему, что бы он следил за invalidatesTags 2. Создаём хук useDeleteProductMutation и импортируем его в Арр. 3. С помощью useDeleteProductMutation получаем deleteProduct, которой, в обработчике событий  handleDeleteProduct мы предаем id, полученный при событии  onClick. 4. Событие onClick происходит на кнопке, которую мы создали для этого.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { useState } from 'react';
-import { useAddProductMutation, useGetGoodsQuery } from './redux'
+import { useAddProductMutation, useGetGoodsQuery, useDeleteProductMutation } from './redux'
 
 function App() {
 	const [count, setCount] = useState('')
 	const [newProduct, setNewProduct] = useState('')
 	const { data = [], isLoading } = useGetGoodsQuery(count);
 	const [addProduct, { isError }] = useAddProductMutation();
+	const [deleteProduct] = useDeleteProductMutation();
 
 	const handleAddProduct = async () => {
 		if (newProduct) {
 			await addProduct({ name: newProduct }).unwrap()
 			setNewProduct('')
 		}
+	}
+
+	const handleDeleteProduct = async (id) => {
+		await deleteProduct(id).unwrap()
+
 	}
 
 	if (isLoading) return <h1>Loading...</h1>
@@ -33,9 +39,12 @@ function App() {
 			<p>Data from server</p>
 			<ul>
 				{data.map(item => (
-					<li key={item.id}>
-						{item.name}
-					</li>
+					<div style={{ display: 'flex', gap: '10px' }} key={Math.floor(Math.random() * 1000)}>
+						<li key={item.id}>
+							{item.name}
+						</li>
+						<button key={Math.floor(Math.random() * 1000)} onClick={() => handleDeleteProduct(item.id)}>x</button>
+					</div>
 				))}
 			</ul>
 		</div>
