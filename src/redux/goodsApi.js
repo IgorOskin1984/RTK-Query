860faa44367da63ccb800cb5ed7448e28a7ab78b
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-13 18:09:02 +0300

    Отрисовываем данные после отправки на сервер. По идее мы отправляем get запрос, после того, как отаравили данные на сервер. Происходит это так: 1. В goodsApi мы прописываем tagTypes: со значением ['Products'] 2. В ендпоинте getGoods мы прописываем providesTags, где сообщаем, что если  { type: 'Products', id: 'LIST' } изменится, нужно отправить повторный get запрос. 3. В endpoint addProduct мы добавляем invalidatesTags, который будет следить за изменениями providesTags

--- src/redux/goodsApi.js
+++ src/redux/goodsApi.js
@@ -2,17 +2,26 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const goodsApi = createApi({
 	reducerPath: 'goodsApi',
+	tagTypes: ['Products'],
 	baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
 	endpoints: (build) => ({
 		getGoods: build.query({
 			query: (count = '') => `goods?${count && `_limit=${count}`}`,
+			providesTags: (result) =>
+				result
+					? [
+						...result.map(({ id }) => ({ type: 'Products', id })),
+						{ type: 'Products', id: 'LIST' },
+					]
+					: [{ type: 'Products', id: 'LIST' }],
 		}),
 		addProduct: build.mutation({
 			query: (body) => ({
 				url: 'goods',
 				method: 'POST',
 				body,
-			})
+			}),
+			invalidatesTags: [{ type: 'Products', id: 'LIST' }],
 		}),
 	})
 });
