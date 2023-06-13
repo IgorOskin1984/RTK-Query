cf02452732117f006e090dcd3ed775de55715e56
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-13 14:56:32 +0300

    Если поменять скобки на круглые, то return можно не писать???

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
 		<div>
 			<p>Data from server</p>
 			<ul>
-				{data.map(item => {
-					return <li key={item.id}>
+				{data.map(item => (
+					<li key={item.id}>
 						{item.name}
 					</li>
-				})}
+				))}
 			</ul>
 		</div>
 	);
